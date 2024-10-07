5b73fff1e31b8c3a0c5c1a6212e09cd7d2015cd2
Developer <123030444+websitenow@users.noreply.github.com>
2024-10-07 19:05:20 -0300

    Delete t.py

--- t.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-proxies = {
-    'http': 'http://127.0.0.1:9000',
-    'https': 'http://127.0.0.1:9000',
-}
-response = requests.get('http://google.com', proxies=proxies)
-print(response)
